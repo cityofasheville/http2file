79a2cdc521c580a5d49e62c0329e22eb4ac9a210
carlyleec <carlyleec@gmail.com>
2014-08-01 10:30:22 -0400

    Update http2file.js
    fixed to pass

--- http2file.js
+++ http2file.js
@@ -15,7 +15,7 @@ var appConfig = {
   urlHost : 'www.publicstuff.com',
   urlPath : '/api/2.0/requests_list?&after_timestamp=1401595200&api_key=i952rk495itu254sx21141j2d3je3x&limit=1000&request_type_id=11339&return_type=json&status=all&verbose=1',
   outputFileLocation : '../htdocs/Graffiti-Dashboard/data/data.js',
-  outputPrefix : 'var = gonnafail ',
+  outputPrefix : 'var = pubstuff ',
   interval : 60000
 }
 
